08a08d11efffc19e532c751e0c5e64c566741d84
MamaWebsign62 <mregnault.netgyver@gmail.com>
2018-06-22 15:46:13 +0200

    menu, burger, présentation

--- index.php
+++ index.php
@@ -18,61 +18,66 @@
   <body>
     <div class="container-fluid">
 <!-- BURGER -->
-      <!-- <nav>
-        <input type="checkbox" id="checkbox-demo">
-        <label for="checkbox-demo">
-          <ul class="menu demo">
-            <li>
-              <a href="#">Accueil</a>
-            </li>
-            <li>
-              <a href="#">Nourrisson</a>
-            </li>
-            <li>
-              <a href="#">Femme enceinte</a>
-            </li>
-            <li>
-              <a href="#">Médecine chinoise</a>
-            </li>
-            <li>
-              <a href="#">Personne âgée</a>
-            </li>
-            <li>
-              <a href="#">Nous contacter</a>
-            </li>
-            <li>
-              <a href="#">Plan d'accès</a>
-            </li>
-          </ul>
-        <span class="toggle">☰</span>
-        </label>
-      </nav> -->
+<div id="menu-burger">
+    <div class="logo">
+      <img src="img/ostheo.png" alt="logo">
+      <div class="burger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+    <ul>
+      <li rel="accueil">
+        <a href="#">Accueil</a>
+      </li>
+      <li rel="nourrisson">
+        <a href="#">Nourrisson</a>
+      </li>
+      <li rel="femme">
+        <a href="#">Femme enceinte</a>
+      </li>
+      <li rel="medecine">
+        <a href="#">Médecine chinoise</a>
+      </li>
+      <li rel="agee">
+        <a href="#">Personne âgée</a>
+      </li>
+      <li rel="contact">
+        <a href="#">Nous contacter</a>
+      </li>
+      <li rel="plan">
+        <a href="#">Plan d'accès</a>
+      </li>
+    </ul>
+</div>
+
 <!-- BURGER -->
-      <div class="menu">
+      <div id="menu">
           <div class="logo">
             <img src="img/ostheo.png" alt="logo">
           </div>
           <ul>
             <li>
-              <a href="#">Accueil</a>
+              <a href="#" rel="accueil">Accueil</a>
             </li>
             <li>
-              <a href="#">Nourrisson</a>
+              <a href="#" rel="nourrisson">Nourrisson</a>
             </li>
             <li>
-              <a href="#">Femme enceinte</a>
+              <a href="#" rel="femme">Femme enceinte</a>
             </li>
             <li>
-              <a href="#">Médecine chinoise</a>
+              <a href="#" rel="medecine">Médecine chinoise</a>
             </li>
             <li>
-              <a href="#">Personne âgée</a>
+              <a href="#" rel="agee">Personne âgée</a>
             </li>
             <li>
-              <a href="#">Nous contacter</a>
+              <a href="#" rel="contact">Nous contacter</a>
             </li>
             <li>
-              <a href="#">Plan d'accès</a>
+              <a href="#"rel="plan">Plan d'accès</a>
             </li>
           </ul>
       </div>
@@ -105,19 +110,13 @@
       </div>
       <!-- PRESENTATION -->
       <div class="presentation">
-        <img src="./img/cabinet.jpg" alt="cabinet-lyon">
-        <h1>Osthéopathe Lyon - Jean Eymare</h1>
-        <br/>
-        <p>Nous vous souhaitons la bienvenue sur le site Internet de mon cabinet d'Ostéopathie de Lyon. Vous y trouverez toutes les informations indispensables et un ensemble de réponses à vos questions sur l'ostéopathie et ses pratiques.</p>
-        <br/>
-        <p>J'ai suivi une formation d'ostéopathe DO, formation agréée par le Ministère de la Santé. </p>
-        <p>Le cursus d'ostéopathie dépasse les 5000 heures cumulant des cours théoriques en anatomie, biomécanique, sémiologie, cours pratiques permettant l'acquisition des savoir-faire spécifiques de l'ostéopathie, trois ans de pratique en clinique ostéopathique ainsi qu'une succession de stages en milieu hospitalier, en club sportif et en entreprise.</p>
-        <br/>
-        <p>Votre ostéopathe à Lyon est en mesure de vous soigner en cas de douleurs articulaires et orthopédiques comme des maux de dos, des problèmes de cervicales, des problèmes lombaires. Le praticien de l'ostéopathie peut également traiter les troubles ayant une cause neurologique, comme les sciatiques, les problèmes d'origine intercostale ou cervico-brachiale . Il obtient aussi d'excellents résultats sur les problèmes ORL, neuro-végétatifs ou concernant les viscères. Il sera aussi bénéfique de vous faire traiter par votre ostéopathe après un traumatisme comme un accident de voiture. Dans certains cas il peut être recommandé de consulter votre ostéopathe sans attendre l'apparition des douleurs.</p>
-        <br/>
-        <p>L'ostéopathie est une pratique manuelle qui s'emploie à déterminer et traiter les troubles fonctionnels qui peuvent toucher des structures de l'organisme : dysfonctionnements de tête, du squelette, etc. Elle traite aussi les sportifs ou les femmes en situation de grossesse. L'ostéopathe considère le corps humain dans son intégralité. Son action consiste à rendre de la capacité de mouvement là où n'y en a plus. L'ostéopathie a parmi ses fondements la découverte de la cause. L'intervention de l'ostéopathe est celle d'un intervenant de première intention, ce qui veut dire que la personne à soigner entre en relation directe avec lui sans passer d'abord par un médecin.</p>
-        <br/>
-        <p>Je reçois mes patients dans mon cabinet à Lyon. Les horaires d'ouverture du cabinet d'ostéopathie sont du Lundi au Vendredi, de 8h à 20h et le samedi, de 8h à 14h sur rendez-vous. Les urgences, en cas de nécessité, peuvent être traitées à tout moment de la journée. Le cabinet est accessible aux personnes à mobilité réduite.</p>
+        <div class="opacity">
+          <h1>Osthéopathe Lyon - Jean Eymare</h1>
+          <br/>
+          <p>Nous vous souhaitons la bienvenue sur le site Internet de mon cabinet d'Ostéopathie de Lyon. Vous y trouverez toutes les informations indispensables et un ensemble de réponses à vos questions sur l'ostéopathie et ses pratiques.</p>
+          <br/>
+          <p>Votre ostéopathe à Lyon est en mesure de vous soigner en cas de douleurs articulaires et orthopédiques comme des maux de dos, des problèmes de cervicales, des problèmes lombaires. Le praticien de l'ostéopathie peut également traiter les troubles ayant une cause neurologique, comme les sciatiques, les problèmes d'origine intercostale ou cervico-brachiale.</p>
+        </div>
       </div>
     </div>
 
--- js/main.js
+++ js/main.js
@@ -1,10 +1,44 @@
+function slider(){
+  $('.bxslider').bxSlider({
+      pager: true,
+      controls: true,
+      speed: 1000,
+      mode: 'fade',
+      auto: true,
+      pause: 5000,
+  });
+}
+
+function menu_active(){
+  var path = window.location.pathname;
+  var location = path.substr(path.lastIndexOf('/')+1);
+  var rel = "";
+  switch (location) {
+    case 'nourrisson.php':
+      rel = "nourrisson";
+    break;
+    case 'femme-enceinte.php':
+      rel = "femme";
+    break;
+    case 'medecine-chinoise.php':
+      rel = "medecine";
+    break;
+    case 'personne-agee.php':
+      rel = "agee";
+    break;
+    case 'contact.php':
+      rel = "contact";
+    break;
+    case 'plan-acces.php':
+      rel = "plan";
+    break;
+    default:
+      rel = "accueil";
+  }
+  $("#menu-burger li[rel='"+rel+"'], #menu a[rel='"+rel+"']").addClass("active");
+}
+
 $(document).ready(function(){
-            $('.bxslider').bxSlider({
-                pager: true,
-                controls: true,
-                speed: 1000,
-                mode: 'fade',
-                auto: true,
-                pause: 5000,
-            });
-        });
+  slider();
+  menu_active();
+});
