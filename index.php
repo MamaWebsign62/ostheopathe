80ea7c91ea278db21075fd6ae6fa7b79ada54d3a
MamaWebsign62 <mregnault.netgyver@gmail.com>
2018-06-22 08:59:00 +0200

    Menu et slider

--- index.php
+++ index.php
@@ -1,71 +1,132 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Site, Osthéopathe">
-    <meta name="keywords" content="HTML, CSS, Bootstrap, JavaScript">
-    <meta name="author" content="Marianne">
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Site, Osthéopathe" />
+    <meta name="keywords" content="HTML, CSS, Bootstrap, JavaScript" />
+    <meta name="author" content="Marianne" />
     <title>Jean Eymare, osthéopathe</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="./js/scrollbox-slider/style-scrollbox.css">
-  <link rel="stylesheet" href="./font-awesome-4.7.0/css/font-awesome.css">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/css/style.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="./js/scrollbox-slider/style-scrollbox.css" />
+    <link rel="stylesheet" href="./font-awesome-4.7.0/css/font-awesome.css" />
+    <link rel="stylesheet" href="/css/style.css" />
   </head>
   <body>
     <div class="container-fluid">
-      <!-- HEADER -->
-      <div class="row">
-        <header class="col-md-12 col-sm-12 col-xs-12">
+<!-- BURGER -->
+      <!-- <nav>
+        <input type="checkbox" id="checkbox-demo">
+        <label for="checkbox-demo">
+          <ul class="menu demo">
+            <li>
+              <a href="#">Accueil</a>
+            </li>
+            <li>
+              <a href="#">Nourrisson</a>
+            </li>
+            <li>
+              <a href="#">Femme enceinte</a>
+            </li>
+            <li>
+              <a href="#">Médecine chinoise</a>
+            </li>
+            <li>
+              <a href="#">Personne âgée</a>
+            </li>
+            <li>
+              <a href="#">Nous contacter</a>
+            </li>
+            <li>
+              <a href="#">Plan d'accès</a>
+            </li>
+          </ul>
+        <span class="toggle">☰</span>
+        </label>
+      </nav> -->
+<!-- BURGER -->
+      <div class="menu">
           <div class="logo">
             <img src="img/ostheo.png" alt="logo">
           </div>
-          <nav class="navbar">
-            <ul>
-              <li>
-                <a href="#">Accueil</a>
-              </li>
-              <li>
-                <a href="#">Nourrisson</a>
-              </li>
-              <li>
-                <a href="#">Femme enceinte</a>
-              </li>
-              <li>
-                <a href="#">Médecine chinoise</a>
-              </li>
-              <li>
-                <a href="#">Personne âgée</a>
-              </li>
-              <li>
-                <a href="#">Nous contacter</a>
-              </li>
-              <li>
-                <a href="#">Plan d'accès</a>
-              </li>
-            </ul>
-          </nav>
-        </header>
+          <ul>
+            <li>
+              <a href="#">Accueil</a>
+            </li>
+            <li>
+              <a href="#">Nourrisson</a>
+            </li>
+            <li>
+              <a href="#">Femme enceinte</a>
+            </li>
+            <li>
+              <a href="#">Médecine chinoise</a>
+            </li>
+            <li>
+              <a href="#">Personne âgée</a>
+            </li>
+            <li>
+              <a href="#">Nous contacter</a>
+            </li>
+            <li>
+              <a href="#">Plan d'accès</a>
+            </li>
+          </ul>
       </div>
-      <!-- SCROLLBOX SLIDER -->
-	    <div id="slider">
 
-        	<div class="slideshow">
-  				 <ul>
-  					 <li><img src="img/slide1.jpg" alt=""></li>
-  					 <li><img src="img/slide2.jpg" alt=""></li>
-  					 <li><img src="img/slide3.jpg" alt=""></li>
-             <li><img src="img/slide4.jpg" alt=""></li>
-  					 <li><img src="img/slide5.jpg" alt=""></li>
-  					 <li><img src="img/slide6.jpg" alt=""></li>
-             <li><img src="img/slide7.jpg" alt=""></li>
-  				 </ul>
-			  </div>
+      <!-- SCROLLBOX SLIDER -->
+
+      <div class="bxslider">
+        <div>
+          <img src="img/slide1.jpg" />
+          <div class="button-slide-left">
+            <a href="page/nourrisson.php">Visitez la page</a>
+          </div>
+        </div>
+        <div>
+          <img src="img/slide2.jpg" />
+          <div class="button-slide-right">
+            <a href="page/femme-enceinte.php">Visitez la page</a>
+          </div>
+        </div>
+        <div>
+          <img src="img/slide3.jpg" />
+          <div class="button-slide-left">
+            <a href="page/medecine-chinoise.php">Visitez la page</a>
+          </div>
+        </div>
+      </div>
+      <!-- BANDEAU -->
+      <div class="bandeau">
+        <p class="bold">Voici quelques spécificités sur mon cabinet</p>
+      </div>
+      <!-- PRESENTATION -->
+      <div class="presentation">
+        <img src="./img/cabinet.jpg" alt="cabinet-lyon">
+        <h1>Osthéopathe Lyon - Jean Eymare</h1>
+        <br/>
+        <p>Nous vous souhaitons la bienvenue sur le site Internet de mon cabinet d'Ostéopathie de Lyon. Vous y trouverez toutes les informations indispensables et un ensemble de réponses à vos questions sur l'ostéopathie et ses pratiques.</p>
+        <br/>
+        <p>J'ai suivi une formation d'ostéopathe DO, formation agréée par le Ministère de la Santé. </p>
+        <p>Le cursus d'ostéopathie dépasse les 5000 heures cumulant des cours théoriques en anatomie, biomécanique, sémiologie, cours pratiques permettant l'acquisition des savoir-faire spécifiques de l'ostéopathie, trois ans de pratique en clinique ostéopathique ainsi qu'une succession de stages en milieu hospitalier, en club sportif et en entreprise.</p>
+        <br/>
+        <p>Votre ostéopathe à Lyon est en mesure de vous soigner en cas de douleurs articulaires et orthopédiques comme des maux de dos, des problèmes de cervicales, des problèmes lombaires. Le praticien de l'ostéopathie peut également traiter les troubles ayant une cause neurologique, comme les sciatiques, les problèmes d'origine intercostale ou cervico-brachiale . Il obtient aussi d'excellents résultats sur les problèmes ORL, neuro-végétatifs ou concernant les viscères. Il sera aussi bénéfique de vous faire traiter par votre ostéopathe après un traumatisme comme un accident de voiture. Dans certains cas il peut être recommandé de consulter votre ostéopathe sans attendre l'apparition des douleurs.</p>
+        <br/>
+        <p>L'ostéopathie est une pratique manuelle qui s'emploie à déterminer et traiter les troubles fonctionnels qui peuvent toucher des structures de l'organisme : dysfonctionnements de tête, du squelette, etc. Elle traite aussi les sportifs ou les femmes en situation de grossesse. L'ostéopathe considère le corps humain dans son intégralité. Son action consiste à rendre de la capacité de mouvement là où n'y en a plus. L'ostéopathie a parmi ses fondements la découverte de la cause. L'intervention de l'ostéopathe est celle d'un intervenant de première intention, ce qui veut dire que la personne à soigner entre en relation directe avec lui sans passer d'abord par un médecin.</p>
+        <br/>
+        <p>Je reçois mes patients dans mon cabinet à Lyon. Les horaires d'ouverture du cabinet d'ostéopathie sont du Lundi au Vendredi, de 8h à 20h et le samedi, de 8h à 14h sur rendez-vous. Les urgences, en cas de nécessité, peuvent être traitées à tout moment de la journée. Le cabinet est accessible aux personnes à mobilité réduite.</p>
       </div>
     </div>
+
+
+
   </body>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.css" rel="stylesheet" />
+    <script type="text/javascript" src="./js/scrollbox-slider/scrollbox.js"></script>
+    <script type="text/javascript" src="./js/main.js"></script>
 </html>
---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+            $('.bxslider').bxSlider({
+                pager: true,
+                controls: true,
+                speed: 1000,
+                mode: 'fade',
+                auto: true,
+                pause: 5000,
+            });
+        });
