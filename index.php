977447acb59d5356fa1d419ce3c08ce7aea2ad46
MamaWebsign62 <mregnault.netgyver@gmail.com>
2018-06-23 11:17:06 +0200

    Page accueil, backtop, footer

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="/css/style.css" />
   </head>
 
-  
+
   <body>
     <div class="container-fluid">
 <!-- BURGER -->
@@ -31,25 +31,25 @@
     </div>
     <ul>
       <li rel="accueil">
-        <a href="#">Accueil</a>
+        <a href="index.php">Accueil</a>
       </li>
       <li rel="nourrisson">
-        <a href="#">Nourrisson</a>
+        <a href="page/nourrisson.php">Nourrisson</a>
       </li>
       <li rel="femme">
-        <a href="#">Femme enceinte</a>
+        <a href="page/femme-enceinte.php">Femme enceinte</a>
       </li>
       <li rel="medecine">
-        <a href="#">Médecine chinoise</a>
+        <a href="page/medecine-chinoise.php">Médecine chinoise</a>
       </li>
       <li rel="agee">
-        <a href="#">Personne âgée</a>
+        <a href="page/personne-agee.php">Personne âgée</a>
       </li>
       <li rel="contact">
-        <a href="#">Nous contacter</a>
+        <a href="page/contact.php">Nous contacter</a>
       </li>
       <li rel="plan">
-        <a href="#">Plan d'accès</a>
+        <a href="page/plan-acces.php">Plan d'accès</a>
       </li>
     </ul>
 </div>
@@ -61,25 +61,25 @@
           </div>
           <ul>
             <li>
-              <a href="#" rel="accueil">Accueil</a>
+              <a href="index.php" rel="accueil">Accueil</a>
             </li>
             <li>
-              <a href="#" rel="nourrisson">Nourrisson</a>
+              <a href="page/nourrisson.php" rel="nourrisson">Nourrisson</a>
             </li>
             <li>
-              <a href="#" rel="femme">Femme enceinte</a>
+              <a href="page/femme-enceinte.php" rel="femme">Femme enceinte</a>
             </li>
             <li>
-              <a href="#" rel="medecine">Médecine chinoise</a>
+              <a href="page/medecine-chinoise.php" rel="medecine">Médecine chinoise</a>
             </li>
             <li>
-              <a href="#" rel="agee">Personne âgée</a>
+              <a href="page/personne-agee.php" rel="agee">Personne âgée</a>
             </li>
             <li>
-              <a href="#" rel="contact">Nous contacter</a>
+              <a href="page/contact.php" rel="contact">Nous contacter</a>
             </li>
             <li>
-              <a href="#"rel="plan">Plan d'accès</a>
+              <a href="page/plan-acces.php"rel="plan">Plan d'accès</a>
             </li>
           </ul>
       </div>
@@ -118,14 +118,14 @@
           <p>Nous vous souhaitons la bienvenue sur le site Internet de mon cabinet d'Ostéopathie de Lyon. Vous y trouverez toutes les informations indispensables et un ensemble de réponses à vos questions sur l'ostéopathie et ses pratiques.</p>
           <br/>
           <div class="button-right">
-            <a href="page/presentation.php">En savoir plus</a>
+            <a href="#qui-sommes-nous">En savoir plus</a>
           </div>
         </div>
       </div>
       <!-- NOURRISSON -->
       <div class="baby">
-        <div class="opacity">
-          <h2>Osthéopathie pour nourrissons</h1>
+        <div class="opacity opacity-nourrisson">
+          <h2>Osthéopathie pour nourrissons</h2>
           <br/>
           <p>Au moyen d'une approche naturelle, l'ostéopathe diagnostiquera et rectifiera les dysfonctionnements mineurs à l'origine des troubles. Grâce aux mains de l'ostéopathe, votre nouveau-né regagnera toute la joie de vivre que vous lui souhaitez.</p>
           <br/>
@@ -136,8 +136,8 @@
       </div>
       <!-- FEMME ENCEINTE -->
       <div class="praignant">
-        <div class="opacity">
-          <h2>Osthéopathie pour femmes enceintes</h1>
+        <div class="opacity opacity-femme">
+          <h2>Osthéopathie pour femmes enceintes</h2>
           <br/>
           <p>Si vous attendez un bébé, notre cabinet d'ostéopathie dispense des consultations adaptées à l'état de grossesse. N'hésitez pas à nous contacter pour davantage d'informations ou bien prendre un rendez-vous.</p>
           <br/>
@@ -146,10 +146,10 @@
           </div>
         </div>
       </div>
-      <!-- NOURRISSON -->
+      <!-- MEDECINE CHINOISE -->
       <div class="medecine">
-        <div class="opacity">
-          <h2>Osthéopathie pour nourrissons</h1>
+        <div class="opacity opacity-medecine">
+          <h2>Médecine chinoise</h2>
           <br/>
           <p>Indéniablement, la Médecine Traditionnelle Chinoise apporte aujourd'hui à notre société une réponse crédible quant aux grands problèmes de santé.</p>
           <br/>
@@ -158,58 +158,95 @@
           </div>
         </div>
       </div>
+      <!-- PERSONNE AGEE -->
+      <div class="agee">
+        <div class="opacity">
+          <h2>Osthéopathie pour personne âgée</h2>
+          <br/>
+          <p>Promouvoir le « mieux vieillir » en maintenant un bon état global, physique, et psychique de l’individu par une action de prévention tout au long de la vie, garantit son capital santé. Par exemple, les traitements ostéopathiques améliorent l’équilibre, diminuent les douleurs ressenties à court et long termes et ainsi participent à l’amélioration de la qualité de vie.</p>
+          <br/>
+          <div class="button-right">
+            <a href="page/personne-agee.php">En savoir plus</a>
+          </div>
+        </div>
+      </div>
+      <!-- QUI SOMMES-NOUS ? -->
+      <div class="qui" id="qui-sommes-nous">
+        <div class="opacity-bg">
+          <h2>Qui sommes-nous ?</h2>
+          <br/>
+          <p>Indéniablement, la Médecine Traditionnelle Chinoise apporte aujourd'hui à notre société une réponse crédible quant aux grands problèmes de santé.</p>
+          <br/>
+          <p>J'ai suivi une formation d'ostéopathe DO, formation agréée par le Ministère de la Santé. Le cursus d'ostéopathie dépasse les 5000 heures cumulant des cours théoriques en anatomie, biomécanique, sémiologie, cours pratiques permettant l'acquisition des savoir-faire spécifiques de l'ostéopathie, trois ans de pratique en clinique ostéopathique ainsi qu'une succession de stages en milieu hospitalier, en club sportif et en entreprise.</p>
+          <br/>
+          <p>Votre ostéopathe à Lyon est en mesure de vous soigner en cas de douleurs articulaires et orthopédiques comme des maux de dos, des problèmes de cervicales, des problèmes lombaires. Le praticien de l'ostéopathie peut également traiter les troubles ayant une cause neurologique, comme les sciatiques, les problèmes d'origine intercostale ou cervico-brachiale . Il obtient aussi d'excellents résultats sur les problèmes ORL, neuro-végétatifs ou concernant les viscères. Il sera aussi bénéfique de vous faire traiter par votre ostéopathe après un traumatisme comme un accident de voiture. Dans certains cas il peut être recommandé de consulter votre ostéopathe sans attendre l'apparition des douleurs.</p>
+          <br/>
+          <p>L'ostéopathie est une pratique manuelle qui s'emploie à déterminer et traiter les troubles fonctionnels qui peuvent toucher des structures de l'organisme : dysfonctionnements de tête, du squelette, etc. Elle traite aussi les sportifs ou les femmes en situation de grossesse. L'ostéopathe considère le corps humain dans son intégralité. Son action consiste à rendre de la capacité de mouvement là où n'y en a plus. L'ostéopathie a parmi ses fondements la découverte de la cause. L'intervention de l'ostéopathe est celle d'un intervenant de première intention, ce qui veut dire que la personne à soigner entre en relation directe avec lui sans passer d'abord par un médecin.</p>
+          <br/>
+          <p>Je reçois mes patients dans mon cabinet à Lyon. <span>Les horaires d'ouverture du cabinet d'ostéopathie sont du Lundi au Vendredi, de 8h à 20h et le samedi, de 8h à 14h sur rendez-vous.</span> Les urgences, en cas de nécessité, peuvent être traitées à tout moment de la journée. Le cabinet est accessible aux personnes à mobilité réduite.</p>
+          <br/>
+        </div>
+      </div>
     </div>
 
     <!-- FOOTER -->
     <footer class="footer">
-            <div class="colon-foot">
-              <div class="info-footer">
-                <ul>
-                  <h3>Nos services</h3>
-                  <li>
-                    <a href="page/index.php" target="_blank">Présentation du cabinet</a>
-                  </li>
-                  <li>
-                    <a href="page/nourrisson.php" target="_blank">Nourrisson</a>
-                  </li>
-                  <li>
-                    <a href="page/femme-enceinte.php" target="_blank">Femme enceinte</a>
-                  </li>
-                  <li>
-                    <a href="page/medecine-chinoise.php" target="_blank">Médecine chinoise</a>
-                  </li>
-                  <li>
-                    <a href="#qui-sommes-nous" target="_blank">Qui sommes-nous ?</a>
-                  </li>
-                </ul>
-                <ul>
-                  <h3>Contactez-nous</h3>
-                  <li>
-                    <a href="page/contact.php">Accédez au formulaire</a>
-                  </li>
-                </ul>
-                <ul>
-                  <h3>Suivez-nous</h3>
-                  <li>
-                    <i class="fa fa-facebook-square" aria-hidden="true">
-                      <a href="#" target="_blank">sur Facebook</a>
-                    </i>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div class="copyright">
-              <i class="fa fa-copyright" aria-hidden="true"></i>
-              Copyright 2018 |
-              <a href="page/mentions.php" target="_blank">Mentions légales</a>
-               | <a href="https://www.ostheopathe-jeaneymare.fr" target="_blank">www.ostheopathe-jeaneymare.fr</a>
-            </div>
-          </footer>
+      <div class="colon-foot">
+        <div class="info-footer">
+          <ul>
+            <h3>Plan du site</h3>
+            <li>
+              <a href="page/index.php">Accueil</a>
+            </li>
+            <li>
+              <a href="page/nourrisson.php" target="_blank">Nourrisson</a>
+            </li>
+            <li>
+              <a href="page/femme-enceinte.php" target="_blank">Femme enceinte</a>
+            </li>
+            <li>
+              <a href="page/medecine-chinoise.php" target="_blank">Médecine chinoise</a>
+            </li>
+            <li>
+              <a href="page/personne-agee.php" target="_blank">Personne âgée</a>
+            </li>
+            <li>
+              <a href="page/contact.php" target="_blank">Contact</a>
+            </li>
+            <li>
+              <a href="page/plan-acces.php">Plan d'accès</a>
+            </li>
+          </ul>
 
-          <!-- BACK-TOP -->
-         <div id="back-top">
-           <a href="#top"><i class="fa fa-chevron-up" aria-hidden="true"></i></a>
-         </div>
+          <ul>
+            <h3>Contactez-nous</h3>
+            <li>
+              <a href="page/contact.php">Accédez au formulaire</a>
+            </li>
+          </ul>
+
+          <ul>
+            <h3>Suivez-nous</h3>
+            <li>
+              <i class="fa fa-facebook-square" aria-hidden="true">
+                <a href="#" target="_blank">sur Facebook</a>
+              </i>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="copyright">
+        <i class="fa fa-copyright" aria-hidden="true"></i>
+        Copyright 2018 |
+          <a href="page/mentions.php" target="_blank">Mentions légales</a>
+          | <a href="https://www.ostheopathe-jeaneymare.fr" target="_blank">www.ostheopathe-jeaneymare.fr</a>
+      </div>
+    </footer>
+
+    <!-- BACK-TOP -->
+    <div id="back-top">
+     <a href="#top"><i class="fa fa-chevron-up" aria-hidden="true"></i></a>
+    </div>
 
 
 
@@ -219,4 +256,5 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.css" rel="stylesheet" />
     <script type="text/javascript" src="./js/scrollbox-slider/scrollbox.js"></script>
     <script type="text/javascript" src="./js/main.js"></script>
+    <script type="text/javascript" src="./js/jq.js"></script>
 </html>
