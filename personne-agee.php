53bc2b67d61df03a35d1c9449882ca7b6a2280b2
MamaWebsign62 <mregnault.netgyver@gmail.com>
2018-06-24 09:58:38 +0200

    Page personne agee

---
+++ personne-agee.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Site, Osthéopathe" />
+    <meta name="keywords" content="HTML, CSS, Bootstrap, JavaScript" />
+    <meta name="author" content="Marianne" />
+    <title>Jean Eymare, osthéopathe</title>
+    <link href="./css/bootstrap.min.css" rel="stylesheet" />
+    <link href="./css/bootstrap-theme.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="./js/scrollbox-slider/style-scrollbox.css" />
+    <link rel="stylesheet" href="./font-awesome-4.7.0/css/font-awesome.css" />
+    <link rel="stylesheet" href="./css/style.css" />
+  </head>
+
+
+  <body>
+    <div class="container-fluid">
+<!-- BURGER -->
+<div id="menu-burger">
+    <div class="logo">
+      <img src="./img/ostheo.png" alt="logo">
+      <div class="burger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+    <ul>
+      <li rel="accueil">
+        <a href="index.php">Accueil</a>
+      </li>
+      <li rel="nourrisson">
+        <a href="nourrisson.php">Nourrisson</a>
+      </li>
+      <li rel="femme">
+        <a href="femme-enceinte.php">Femme enceinte</a>
+      </li>
+      <li rel="medecine">
+        <a href="medecine-chinoise.php">Médecine chinoise</a>
+      </li>
+      <li rel="agee">
+        <a href="personne-agee.php">Personne âgée</a>
+      </li>
+      <li rel="contact">
+        <a href="contact.php">Nous contacter</a>
+      </li>
+      <li rel="plan">
+        <a href="plan-acces.php">Plan d'accès</a>
+      </li>
+    </ul>
+</div>
+
+<!-- MENU -->
+      <div id="menu">
+          <div class="logo">
+            <img src="img/ostheo.png" alt="logo">
+          </div>
+          <ul>
+            <li>
+              <a href="index.php" rel="accueil">Accueil</a>
+            </li>
+            <li>
+              <a href="nourrisson.php" rel="nourrisson">Nourrisson</a>
+            </li>
+            <li>
+              <a href="femme-enceinte.php" rel="femme">Femme enceinte</a>
+            </li>
+            <li>
+              <a href="medecine-chinoise.php" rel="medecine">Médecine chinoise</a>
+            </li>
+            <li>
+              <a href="personne-agee.php" rel="agee">Personne âgée</a>
+            </li>
+            <li>
+              <a href="contact.php" rel="contact">Nous contacter</a>
+            </li>
+            <li>
+              <a href="plan-acces.php"rel="plan">Plan d'accès</a>
+            </li>
+          </ul>
+      </div>
+
+      <!-- SCROLLBOX SLIDER -->
+
+      <div class="bxslider">
+        <div>
+          <img src="./img/slide1.jpg"/>
+          <div class="button-slide-left">
+            <a href="nourrisson.php">En savoir plus</a>
+          </div>
+        </div>
+        <div>
+          <img src="./img/slide2.jpg"/>
+          <div class="button-slide-right">
+            <a href="femme-enceinte.php">Découvrir l'offre</a>
+          </div>
+        </div>
+        <div>
+          <img src="./img/slide3.jpg"/>
+          <div class="button-slide-left">
+            <a href="medecine-chinoise.php">En savoir plus</a>
+          </div>
+        </div>
+      </div>
+      <!-- BANDEAU -->
+      <div class="bandeau">
+        <p class="bold">Le cabinet est situé 26 rue de la République à Lyon</p>
+        <p class="bold">Pour nous joindre : appelez-le 04.74.01.02.03.</p>
+      </div>
+
+      <!-- SECTION -->
+      <section id="article" class="article-personne">
+        <h1>Osthéopathe pour personne âgée, Jean Eymare</h1>
+
+        <div class="tribox-personne">
+           <img src="./img/personne-agee1.jpg" width="298" height="199" alt="photo-personne-agee">
+           <div class="titre2">
+               <h2><i class="fa fa-circle" aria-hidden="true"></i>Bien vieillir avec l'ostéopathie</h2>
+               <div class="paragraph-box medecine-chinoise">
+                 <p>Au fil des ans notre corps est sujet à des transformations qui, progressivement, entrainent des diminutions de l’ensemble de ses fonctions, favorisant l’apparition d’incapacités.</p>
+                 <p>Au fil des ans notre corps est sujet à des transformations qui, progressivement, entrainent des diminutions de l’ensemble de ses fonctions, favorisant l’apparition d’incapacités.</p>
+                 <p>Une personne âgée peut se voir au fur et à mesure privée de ses repères, de son potentiel à communiquer correctement avec son entourage et être exposée aux chutes. Elle se retrouve ainsi restreinte dans les actes de sa vie quotidienne.</p>
+                 <p>Être suivi le plus tôt et le plus régulièrement possible par un ostéopathe permet de retrouver et de renforcer ses fonctions en travaillant sur la mobilité générale de son corps.</p>
+                 <p>Promouvoir le « mieux vieillir » en maintenant un bon état global, physique, et psychique de l’individu par une action de prévention tout au long de la vie, garantit son capital santé. Par exemple, les traitements ostéopathiques améliorent l’équilibre, diminuent les douleurs ressenties à court et long termes et ainsi participent à l’amélioration de la qualité de vie.</p>
+               </div>
+           </div>
+         </div>
+
+        <div class="tribox-personne tribox2">
+          <div class="titre3">
+            <h2><i class="fa fa-circle" aria-hidden="true"></i>La prévention pour promouvoir le « mieux vieillir »</h2>
+            <div class="paragraph-box2 medecine-chinoise">
+              <p>La Prévention primaire a pour but de maintenir en bonne santé. Effectuée régulièrement, elle permet d’éviter l’apparition de troubles fonctionnels. Le senior entretien souplesse et équilibre pour pouvoir vivre sans contrainte.</p>
+              <p>La Prévention secondaire soigne le trouble fonctionnel qui est apparu avec le temps ou suite à un traumatisme corporel, et permet d’éviter rechutes et récidives. Lorsque qu’un trouble fonctionnel survient, le suivi ostéopathique permet d’éviter la propagation des maux apparus en d’autres points du corps.</p>
+              <p>La Prévention Tertiaire minimise une pathologie déjà présente et cherche à diminuer l’invalidité du patient. Lorsque la maladie est présente, le praticien soutient la santé en minorant les différents troubles annexes et conjoints. Une entorse de la cheville, un lumbago ou une douleur ne peuvent que rajouter à la peine d’une personne atteinte de Parkinson, d’insuffisance respiratoire ou d’un cancer.</p>
+            </div>
+          </div>
+
+          <img src="./img/personne-agee2.jpg" width="298" height="199" alt="photo-personne-agee">
+        </div>
+
+        <div class="tribox-personne">
+           <img src="./img/personne-agee3.jpg" width="298" height="199" alt="photo-personne-agee">
+           <div class="titre2">
+               <h2><i class="fa fa-circle" aria-hidden="true"></i>L’ostéopathie repose sur trois concepts originaux</h2>
+               <div class="paragraph-box medecine-chinoise">
+                 <p>La main, outil d’analyse et de soins; la prise en compte de la globalité de l’individu et le principe d’équilibre tissulaire.</p>
+                 <p>Elle nécessite des compétences spécifiques, une connaissance approfondie du fonctionnement du corps humain et des interactions entre chacun de ses systèmes.</p>
+                 <p>Elle prévient et soigne de nombreux troubles physiques et agit également aux plans nerveux, fonctionnel et psychique.</p>
+                 <p>Elle aide chacun à gérer, de manière responsable et autonome, son “capital vie” pour un mieux-être au quotidien.</p>
+               </div>
+           </div>
+         </div>
+
+      </section>
+    </div>
+
+    <!-- FOOTER -->
+    <footer class="footer">
+      <div class="colon-foot">
+        <!-- <div class="info-footer"> -->
+          <ul>
+            <h3>Plan du site</h3>
+            <li>
+              <a href="index.php">Accueil</a>
+            </li>
+            <li>
+              <a href="nourrisson.php" target="_blank">Nourrisson</a>
+            </li>
+            <li>
+              <a href="femme-enceinte.php" target="_blank">Femme enceinte</a>
+            </li>
+            <li>
+              <a href="medecine-chinoise.php" target="_blank">Médecine chinoise</a>
+            </li>
+            <li>
+              <a href="personne-agee.php" target="_blank">Personne âgée</a>
+            </li>
+            <li>
+              <a href="contact.php" target="_blank">Contact</a>
+            </li>
+            <li>
+              <a href="plan-acces.php">Plan d'accès</a>
+            </li>
+          </ul>
+
+          <ul>
+            <h3>Contactez-nous</h3>
+            <li>
+              <a href="contact.php">Accédez au formulaire</a>
+            </li>
+          </ul>
+
+          <ul>
+            <h3>Suivez-nous</h3>
+            <li>
+              <i class="fa fa-facebook-square" aria-hidden="true">
+                <a href="#" target="_blank">sur Facebook</a>
+              </i>
+            </li>
+          </ul>
+        <!-- </div> -->
+      </div>
+      <div class="copyright">
+        <i class="fa fa-copyright" aria-hidden="true"></i>
+        Copyright 2018 |
+          <a href="mentions.php" target="_blank">Mentions légales</a>
+          | <a href="https://www.ostheopathe-jeaneymare.fr" target="_blank">www.ostheopathe-jeaneymare.fr</a>
+      </div>
+    </footer>
+
+    <!-- BACK-TOP -->
+    <div id="back-top">
+     <a href="#top"><i class="fa fa-chevron-up" aria-hidden="true"></i></a>
+    </div>
+
+
+
+  </body>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.css" rel="stylesheet" />
+    <script type="text/javascript" src="./js/scrollbox-slider/scrollbox.js"></script>
+    <script type="text/javascript" src="./js/main.js"></script>
+    <script type="text/javascript" src="./js/jq.js"></script>
+</html>
