a50a1d16efbb137645abef8e21edbc731fc34d79
MamaWebsign62 <mregnault.netgyver@gmail.com>
2018-06-24 09:15:00 +0200

    Page medecine chinoise

---
+++ medecine-chinoise.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Site, Osthéopathe" />
+    <meta name="keywords" content="HTML, CSS, Bootstrap, JavaScript" />
+    <meta name="author" content="Marianne" />
+    <title>Jean Eymare, osthéopathe</title>
+    <link href="./css/bootstrap.min.css" rel="stylesheet" />
+    <link href="./css/bootstrap-theme.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="./js/scrollbox-slider/style-scrollbox.css" />
+    <link rel="stylesheet" href="./font-awesome-4.7.0/css/font-awesome.css" />
+    <link rel="stylesheet" href="./css/style.css" />
+  </head>
+
+
+  <body>
+    <div class="container-fluid">
+<!-- BURGER -->
+<div id="menu-burger">
+    <div class="logo">
+      <img src="./img/ostheo.png" alt="logo">
+      <div class="burger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+    <ul>
+      <li rel="accueil">
+        <a href="index.php">Accueil</a>
+      </li>
+      <li rel="nourrisson">
+        <a href="nourrisson.php">Nourrisson</a>
+      </li>
+      <li rel="femme">
+        <a href="femme-enceinte.php">Femme enceinte</a>
+      </li>
+      <li rel="medecine">
+        <a href="medecine-chinoise.php">Médecine chinoise</a>
+      </li>
+      <li rel="agee">
+        <a href="personne-agee.php">Personne âgée</a>
+      </li>
+      <li rel="contact">
+        <a href="contact.php">Nous contacter</a>
+      </li>
+      <li rel="plan">
+        <a href="plan-acces.php">Plan d'accès</a>
+      </li>
+    </ul>
+</div>
+
+<!-- MENU -->
+      <div id="menu">
+          <div class="logo">
+            <img src="img/ostheo.png" alt="logo">
+          </div>
+          <ul>
+            <li>
+              <a href="index.php" rel="accueil">Accueil</a>
+            </li>
+            <li>
+              <a href="nourrisson.php" rel="nourrisson">Nourrisson</a>
+            </li>
+            <li>
+              <a href="femme-enceinte.php" rel="femme">Femme enceinte</a>
+            </li>
+            <li>
+              <a href="medecine-chinoise.php" rel="medecine">Médecine chinoise</a>
+            </li>
+            <li>
+              <a href="personne-agee.php" rel="agee">Personne âgée</a>
+            </li>
+            <li>
+              <a href="contact.php" rel="contact">Nous contacter</a>
+            </li>
+            <li>
+              <a href="plan-acces.php"rel="plan">Plan d'accès</a>
+            </li>
+          </ul>
+      </div>
+
+      <!-- SCROLLBOX SLIDER -->
+
+      <div class="bxslider">
+        <div>
+          <img src="./img/slide1.jpg"/>
+          <div class="button-slide-left">
+            <a href="nourrisson.php">En savoir plus</a>
+          </div>
+        </div>
+        <div>
+          <img src="./img/slide2.jpg"/>
+          <div class="button-slide-right">
+            <a href="femme-enceinte.php">Découvrir l'offre</a>
+          </div>
+        </div>
+        <div>
+          <img src="./img/slide3.jpg"/>
+          <div class="button-slide-left">
+            <a href="medecine-chinoise.php">En savoir plus</a>
+          </div>
+        </div>
+      </div>
+      <!-- BANDEAU -->
+      <div class="bandeau">
+        <p class="bold">Le cabinet est situé 26 rue de la République à Lyon</p>
+        <p class="bold">Pour nous joindre : appelez-le 04.74.01.02.03.</p>
+      </div>
+
+      <!-- SECTION -->
+      <section id="article" class="article-medecine">
+        <h1>Médecine chinoise par Jean Eymare, ostéopathe</h1>
+
+        <div class="tribox-medecine">
+           <img src="./img/medecine-chinoise2.jpg" width="298" height="199" alt="photo-medecine-chinoise">
+           <div class="titre2">
+               <h2><i class="fa fa-circle" aria-hidden="true"></i>Comment est apparue la médecine chinoise en Occident ?</h2>
+               <div class="paragraph-box medecine-chinoise">
+                 <p>Dans le courant du XIX° siècle, grâce aux jésuites revenant de Chine, la Médecine Traditionnelle Chinoise est apparue timidement en Occident, et en particulier en France. Même si elle s'est rapidement répandue dans le monde, ce n'est pourtant que dans les années 1970 que cette science a pris chez nous son véritable essor, tant au niveau de sa pratique que dans l'organisation de son enseignement. De nos jours, elle est donc encore relativement peu connue. Elle est nommée par les uns "médecine parallèle", par d'autres "douce", "alternative" ou encore "complémentaire", sans savoir d'ailleurs très bien ce que recouvrent ces expressions.</p>
+                 <p>Ce site a pour but de faire découvrir (ou redécouvrir) à nos contemporains ce type de médecine non seulement très riche culturellement et scientifiquement, mais aussi très efficace et éthiquement juste. Indéniablement, la Médecine Traditionnelle Chinoise apporte aujourd'hui à notre société une réponse crédible quant aux grands problèmes de santé.</p>
+               </div>
+           </div>
+         </div>
+
+        <div class="tribox-medecine tribox2">
+          <div class="titre3">
+            <h2><i class="fa fa-circle" aria-hidden="true"></i>A quoi correspondent les points d’acupuncture ?</h2>
+            <div class="paragraph-box2 medecine-chinoise">
+              <p>Il s’agit d’endroits très précis (moins d’un millimètre carré) situés sur les méridiens dans leur partie superficielle.</p>
+              <p>Il en existe plus de 400, mais un praticien n’en utilise habituellement que 150 environ et une centaine dans sa pratique courante.</p>
+              <p>Plusieurs études scientifiques ont montré qu’à cet endroit précis de la peau, la différence de potentiel entre la partie superficielle et la partie profonde est significativement différente d’un autre endroit pris au hasard. Ceci n’explique évidemment rien, mais montre tout de même qu’un phénomène très particulier existe.</p>
+            </div>
+          </div>
+
+          <img src="./img/medecine-chinoise3.jpg" width="298" height="199" alt="photo-medecine-chinoise">
+        </div>
+
+        <div class="tribox-medecine tribox2">
+          <div class="titre3">
+            <h2><i class="fa fa-circle" aria-hidden="true"></i>Combien d'aiguilles sont-elles posées lors d'une séance ?</h2>
+            <div class="paragraph-box2 medecine-chinoise">
+              <p>Rappelons que la régularisation énergétique fait travailler l’organisme dans un certain sens, afin d’arriver à une harmonisation. De ce fait, on comprend bien qu’il n’est pas possible de poser de trop nombreuses aiguilles dans la même séance, pour ne pas faire travailler l’organisme dans trop de directions simultanément.</p>
+              <p>Habituellement, en Occident, le praticien ne pose pas plus de 7 à 8 aiguilles par séance, pour ne pas trop fatiguer le patient et ainsi optimiser la réponse de l’organisme à la stimulation acupuncturale.</p>
+              <p>Le nombre d’aiguilles posées dépend aussi d’autres facteurs laissés à l’appréciation du praticien : âge du malade, chronicité de la maladie, saison, hygiène de vie, etc.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="tribox-medecine">
+           <img src="./img/medecine-chinoise4.jpg" width="298" height="199" alt="photo-medecine-chinoise">
+           <div class="titre2">
+               <h2><i class="fa fa-circle" aria-hidden="true"></i>Combien de temps les aiguilles sont-elles laissées en place ?</h2>
+               <div class="paragraph-box medecine-chinoise">
+                 <p>Traditionnellement, les aiguilles ne sont laissées en place que quelques secondes pour obtenir une dispersion ; si le praticien veut obtenir un effet de tonification, les aiguilles resteront posées quelques minutes.</p>
+                 <p>Si une aiguille reste placée pendant de nombreuses minutes, se produit alors un effet d’«harmonisation» (technique utilisée par exemple lorsque deux fonctions ne sont pas en harmonie entre elles).</p>
+               </div>
+           </div>
+         </div>
+
+         <div class="tribox-medecine tribox2">
+           <div class="titre3">
+             <h2><i class="fa fa-circle" aria-hidden="true"></i>Existe-t-il des risques de contamination ?</h2>
+             <div class="paragraph-box2 medecine-chinoise">
+               <p>Non. Depuis longtemps, tous les praticiens sérieux de MTC utilisent des aiguilles stériles à usage unique. Ceci permet non seulement d'éviter la moindre infection, mais aussi de ne pas provoquer de douleur importante lors de la poncture, la pointe de l'aiguille étant très affûtée.</p>
+             </div>
+           </div>
+         </div>
+
+      </section>
+    </div>
+
+    <!-- FOOTER -->
+    <footer class="footer">
+      <div class="colon-foot">
+        <!-- <div class="info-footer"> -->
+          <ul>
+            <h3>Plan du site</h3>
+            <li>
+              <a href="index.php">Accueil</a>
+            </li>
+            <li>
+              <a href="nourrisson.php" target="_blank">Nourrisson</a>
+            </li>
+            <li>
+              <a href="femme-enceinte.php" target="_blank">Femme enceinte</a>
+            </li>
+            <li>
+              <a href="medecine-chinoise.php" target="_blank">Médecine chinoise</a>
+            </li>
+            <li>
+              <a href="personne-agee.php" target="_blank">Personne âgée</a>
+            </li>
+            <li>
+              <a href="contact.php" target="_blank">Contact</a>
+            </li>
+            <li>
+              <a href="plan-acces.php">Plan d'accès</a>
+            </li>
+          </ul>
+
+          <ul>
+            <h3>Contactez-nous</h3>
+            <li>
+              <a href="contact.php">Accédez au formulaire</a>
+            </li>
+          </ul>
+
+          <ul>
+            <h3>Suivez-nous</h3>
+            <li>
+              <i class="fa fa-facebook-square" aria-hidden="true">
+                <a href="#" target="_blank">sur Facebook</a>
+              </i>
+            </li>
+          </ul>
+        <!-- </div> -->
+      </div>
+      <div class="copyright">
+        <i class="fa fa-copyright" aria-hidden="true"></i>
+        Copyright 2018 |
+          <a href="mentions.php" target="_blank">Mentions légales</a>
+          | <a href="https://www.ostheopathe-jeaneymare.fr" target="_blank">www.ostheopathe-jeaneymare.fr</a>
+      </div>
+    </footer>
+
+    <!-- BACK-TOP -->
+    <div id="back-top">
+     <a href="#top"><i class="fa fa-chevron-up" aria-hidden="true"></i></a>
+    </div>
+
+
+
+  </body>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.css" rel="stylesheet" />
+    <script type="text/javascript" src="./js/scrollbox-slider/scrollbox.js"></script>
+    <script type="text/javascript" src="./js/main.js"></script>
+    <script type="text/javascript" src="./js/jq.js"></script>
+</html>
