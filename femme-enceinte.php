62147f6f4885fa898c9dec3347ae33e143319442
MamaWebsign62 <mregnault.netgyver@gmail.com>
2018-06-24 08:22:04 +0200

    Page femme enceinte

---
+++ femme-enceinte.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Site, Osthéopathe" />
+    <meta name="keywords" content="HTML, CSS, Bootstrap, JavaScript" />
+    <meta name="author" content="Marianne" />
+    <title>Jean Eymare, osthéopathe</title>
+    <link href="./css/bootstrap.min.css" rel="stylesheet" />
+    <link href="./css/bootstrap-theme.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="./js/scrollbox-slider/style-scrollbox.css" />
+    <link rel="stylesheet" href="./font-awesome-4.7.0/css/font-awesome.css" />
+    <link rel="stylesheet" href="./css/style.css" />
+  </head>
+
+
+  <body>
+    <div class="container-fluid">
+<!-- BURGER -->
+<div id="menu-burger">
+    <div class="logo">
+      <img src="./img/ostheo.png" alt="logo">
+      <div class="burger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+    <ul>
+      <li rel="accueil">
+        <a href="index.php">Accueil</a>
+      </li>
+      <li rel="nourrisson">
+        <a href="nourrisson.php">Nourrisson</a>
+      </li>
+      <li rel="femme">
+        <a href="femme-enceinte.php">Femme enceinte</a>
+      </li>
+      <li rel="medecine">
+        <a href="medecine-chinoise.php">Médecine chinoise</a>
+      </li>
+      <li rel="agee">
+        <a href="personne-agee.php">Personne âgée</a>
+      </li>
+      <li rel="contact">
+        <a href="contact.php">Nous contacter</a>
+      </li>
+      <li rel="plan">
+        <a href="plan-acces.php">Plan d'accès</a>
+      </li>
+    </ul>
+</div>
+
+<!-- MENU -->
+      <div id="menu">
+          <div class="logo">
+            <img src="img/ostheo.png" alt="logo">
+          </div>
+          <ul>
+            <li>
+              <a href="index.php" rel="accueil">Accueil</a>
+            </li>
+            <li>
+              <a href="nourrisson.php" rel="nourrisson">Nourrisson</a>
+            </li>
+            <li>
+              <a href="femme-enceinte.php" rel="femme">Femme enceinte</a>
+            </li>
+            <li>
+              <a href="medecine-chinoise.php" rel="medecine">Médecine chinoise</a>
+            </li>
+            <li>
+              <a href="personne-agee.php" rel="agee">Personne âgée</a>
+            </li>
+            <li>
+              <a href="contact.php" rel="contact">Nous contacter</a>
+            </li>
+            <li>
+              <a href="plan-acces.php"rel="plan">Plan d'accès</a>
+            </li>
+          </ul>
+      </div>
+
+      <!-- SCROLLBOX SLIDER -->
+
+      <div class="bxslider">
+        <div>
+          <img src="./img/slide1.jpg"/>
+          <div class="button-slide-left">
+            <a href="nourrisson.php">En savoir plus</a>
+          </div>
+        </div>
+        <div>
+          <img src="./img/slide2.jpg"/>
+          <div class="button-slide-right">
+            <a href="femme-enceinte.php">Découvrir l'offre</a>
+          </div>
+        </div>
+        <div>
+          <img src="./img/slide3.jpg"/>
+          <div class="button-slide-left">
+            <a href="medecine-chinoise.php">En savoir plus</a>
+          </div>
+        </div>
+      </div>
+      <!-- BANDEAU -->
+      <div class="bandeau">
+        <p class="bold">Le cabinet est situé 26 rue de la République à Lyon</p>
+        <p class="bold">Pour nous joindre : appelez-le 04.74.01.02.03.</p>
+      </div>
+
+      <!-- SECTION -->
+      <section id="article" class="article-femme">
+        <h1>Ostéopathe pour femme enceinte, Jean Eymare</h1>
+
+        <div class="tribox-femme">
+           <img src="./img/femme-enceinte2.jpg" width="298" height="199" alt="photo-femme-enceinte">
+           <div class="titre2">
+               <h2><i class="fa fa-circle" aria-hidden="true"></i>Quelles modifications subit votre corps pendant la grossesse ?</h2>
+               <div class="paragraph-box">
+                 <p>Si vous attendez un bébé, notre cabinet d'ostéopathie dispense des consultations adaptées à l'état de grossesse.</p>
+                 <p>Le fait d'attendre un bébé cause des transformations importantes dans votre corps. Des changements hormonaux vont causer des modifications au niveau des tissus du corps. Les tissus deviennent plus souples au moment de la grossesse pour faciliter le passage du futur bébé au niveau du bassin.</p>
+                 <p>Le poids et la forme de votre ventre vont causer un changement de posture. Le centre de gravité de votre corps va bouger et se positionner plus en avant. La grossesse va également provoquer une hyper-pression dans la région de la cavité abdominale ; les viscères devront alors trouver une nouvelle place.</p>
+                 <p>Toutes ces modifications vont provoquer de nombreux dysfonctionnements entraînant des douleurs que l'ostéopathe saura calmer.</p>
+               </div>
+           </div>
+         </div>
+
+        <div class="tribox-femme tribox2 ">
+          <div class="titre3">
+            <h2><i class="fa fa-circle" aria-hidden="true"></i>Pour quelles raisons est-il intéressant de consulter un ostéopathe pendant votre grossesse ?</h2>
+            <p>Par conséquent, une femme enceinte peut faire appel à un ostéopathe pour de nombreuses raisons : mécaniques, mais aussi digestives, circulatoires, etc. Les douleurs mécaniques, comme les lombalgies, sont dues à tous les changements qui apparaissent pendant la grossesse. La prise de poids et les forces antérieures qui viennent tirer sur les lombaires (insertions des fascias sur les vertèbres) accentuent la cambrure lombaire. La femme enceinte souffrira aussi fréquemment de sciatalgie ou sciatique. La sciatique est due principalement à un pincement du nerf sciatique. Il peut être comprimé lors de son passage dans le muscle pyramidal. Les douleurs du coccyx et du bassin sont aussi assez fréquentes.</p>
+            <p>Une femme enceinte pourra aussi consulter un ostéopathe si elle est sujette à des troubles digestifs comme des constipations, des ballonnements, des diarrhées, des reflux gastro-oesophagiens ou des brûlures d'estomac.</p>
+            <p>Vous pouvez aussi consulter un ostéopathe pendant votre grossesse pour des troubles circulatoires, c'est à dire des migraines, des oedèmes au niveau des membres inférieurs ou des sensations de lourdeur au niveau des jambes.</p>
+            <p>Même en l'absence de troubles déclarés, une femme enceinte peut consulter un ostéopathe à titre préventif. Une telle consultation permet d'éviter la survenance des troubles évoqués ci-dessus et de bénéficier de séances de préparation à l'accouchement.</p>
+          </div>
+
+          <img src="./img/femme-enceinte3.jpg" width="298" height="199" alt="photo-femme-enceinte">
+        </div>
+
+        <div class="tribox-femme tribox3">
+          <img src="./img/femme-enceinte4.jpg" width="298" height="199" alt="photo-femme-enceinte">
+
+          <div class="titre4">
+              <h2><i class="fa fa-circle" aria-hidden="true"></i>Comment se déroule la consultation ?</h2>
+              <div>
+                <p>L'ostéopathe va dans un premier temps observer la patiente pour étudier comment elle se tient et examiner ses courbures. Il va ensuite regarder spécialement les articulations au niveau du bassin et identifier des problèmes ou blocages potentiels. Il va enfin vérifier si les mobilités du thorax, de l'abdomen et des structures crâniennes sont bonnes.</p>
+                <p>La totalité des techniques auxquelles a recours l'ostéopathie sont ajustées à chaque patiente et à chaque contexte de consultation. Elles sont bien entendu non invasives pour le bébé. On peut consulter un ostéopathe dès le troisième mois de grossesse y compris en préventif. En effet, en général les douleurs apparaissent à partir du 6ème mois de grossesse. Une à deux séances sont alors nécessaires pour traiter la cause des douleurs, puis une à deux consultations permettent de préparer l'accouchement.</p>
+                <p>N'hésitez pas à nous contacter pour davantage d'informations ou bien prendre un rendez-vous.</p>
+              </div>
+          </div>
+        </div>
+      </section>
+    </div>
+
+    <!-- FOOTER -->
+    <footer class="footer">
+      <div class="colon-foot">
+        <!-- <div class="info-footer"> -->
+          <ul>
+            <h3>Plan du site</h3>
+            <li>
+              <a href="index.php">Accueil</a>
+            </li>
+            <li>
+              <a href="nourrisson.php" target="_blank">Nourrisson</a>
+            </li>
+            <li>
+              <a href="femme-enceinte.php" target="_blank">Femme enceinte</a>
+            </li>
+            <li>
+              <a href="medecine-chinoise.php" target="_blank">Médecine chinoise</a>
+            </li>
+            <li>
+              <a href="personne-agee.php" target="_blank">Personne âgée</a>
+            </li>
+            <li>
+              <a href="contact.php" target="_blank">Contact</a>
+            </li>
+            <li>
+              <a href="plan-acces.php">Plan d'accès</a>
+            </li>
+          </ul>
+
+          <ul>
+            <h3>Contactez-nous</h3>
+            <li>
+              <a href="contact.php">Accédez au formulaire</a>
+            </li>
+          </ul>
+
+          <ul>
+            <h3>Suivez-nous</h3>
+            <li>
+              <i class="fa fa-facebook-square" aria-hidden="true">
+                <a href="#" target="_blank">sur Facebook</a>
+              </i>
+            </li>
+          </ul>
+        <!-- </div> -->
+      </div>
+      <div class="copyright">
+        <i class="fa fa-copyright" aria-hidden="true"></i>
+        Copyright 2018 |
+          <a href="mentions.php" target="_blank">Mentions légales</a>
+          | <a href="https://www.ostheopathe-jeaneymare.fr" target="_blank">www.ostheopathe-jeaneymare.fr</a>
+      </div>
+    </footer>
+
+    <!-- BACK-TOP -->
+    <div id="back-top">
+     <a href="#top"><i class="fa fa-chevron-up" aria-hidden="true"></i></a>
+    </div>
+
+
+
+  </body>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.css" rel="stylesheet" />
+    <script type="text/javascript" src="./js/scrollbox-slider/scrollbox.js"></script>
+    <script type="text/javascript" src="./js/main.js"></script>
+    <script type="text/javascript" src="./js/jq.js"></script>
+</html>
